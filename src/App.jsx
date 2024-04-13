fffbe9ff3676bba06079541d30a6a9f918e32f61
Fernando-MV7 <huviel.fer@gmail.com>
2024-04-13 17:01:04 -0400

    pregunta_2

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,16 @@
 import './App.css'
-//import { Button } from './components/Button'
-import { WapperList } from './components/WrapperList';
-import { List } from './components/List';
+import { Button } from './components/Button'
+
 function App() {
 
-  //const buttonText = "count : ";
-
-  return (
-    <>
-      <WapperList>
-        <List/>
-      </WapperList>
-    </>
-  )
+  const buttonText = "count : ";
   
-  /*return (
+  return (
     <>
       <h1>SEMINARIO DE SISTEMAS</h1>
       <Button text={buttonText}/>
     </>
-  )*/
+  )
 }
 
 export default App
